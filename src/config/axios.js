164902f7b4ee701bfeeff9650d15fd78292af68e
Adolfo <adolfo.onrubia@wanna.es>
2019-04-19 19:44:58 +0200

    chore(axios): update api url

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.headers['CB-ACCESS-KEY'] = 'Z0ESHoXU4EEG3LkI';
 axios.defaults.headers['CB-ACCESS-TIMESTAMP'] = Date.now();
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'test') {
   axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 } else {
   axios.defaults.baseURL = process.env.REACT_APP_LOCAL_API_URL;
