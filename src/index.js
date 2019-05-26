92254ac46c7a51969d3c91eed615138aa3868976
Adolfo <adolfo.onrubia@wanna.es>
2019-05-26 13:40:58 +0200

    fix(serviceWorket): unregister worker

--- src/index.js
+++ src/index.js
@@ -37,15 +37,15 @@ const Root = () => (
 
 ReactDOM.render(<Root />, document.getElementById('root'));
 
-const config = {
-  onUpdate(registration) {
-    store.dispatch({ type: NEW_APP_AVAILABLE.SET });
-    console.log('onUpdate registration', registration);
-  },
-  onSuccess(registration) {
-    store.dispatch({ type: NEW_APP_AVAILABLE.SET });
-    console.log('onSuccess registration', registration);
-  },
-};
+// const config = {
+//   onUpdate(registration) {
+//     store.dispatch({ type: NEW_APP_AVAILABLE.SET });
+//     console.log('onUpdate registration', registration);
+//   },
+//   onSuccess(registration) {
+//     store.dispatch({ type: NEW_APP_AVAILABLE.SET });
+//     console.log('onSuccess registration', registration);
+//   },
+// };
 // serviceWorker.register(config);
 serviceWorker.unregister();
