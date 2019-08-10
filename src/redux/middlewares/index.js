1e251c1385970562d6b9a23ccd7f3ad28187421d
Adolfo <onrubia78@gmail.com>
2019-08-10 13:50:48 +0200

    add custom middleware

--- src/redux/middlewares/index.js
+++ src/redux/middlewares/index.js
@@ -2,8 +2,14 @@ import { loadingBarMiddleware } from 'react-redux-loading-bar';
 import thunkMiddleware from 'redux-thunk';
 
 const promiseTypeSuffixes = ['REQUEST', 'SUCCESS', 'FAILED'];
+const myMiddleware = (state) => () => next => {
+  console.log('state =>', state);
+  next();
+};
 
 export default [
   thunkMiddleware,
+  myMiddleware,
   loadingBarMiddleware({ promiseTypeSuffixes }),
 ];
+
