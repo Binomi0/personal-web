958e2c50683a94621d22cd57d620b100e7e0343c
Adolfo Onrubia <adolfo@onrubia.es>
2019-08-17 13:00:06 +0200

    resolve conflicts

--- src/redux/middlewares/index.js
+++ src/redux/middlewares/index.js
@@ -2,14 +2,17 @@ import { loadingBarMiddleware } from 'react-redux-loading-bar';
 import thunkMiddleware from 'redux-thunk';
 
 const promiseTypeSuffixes = ['REQUEST', 'SUCCESS', 'FAILED'];
-const myMiddleware = (state) => () => next => {
-  console.log('state =>', state);
-  next();
+
+const logger = (store) => (next) => (action) => {
+  console.log('dispatching', action);
+  let result = next(action);
+  console.log('next state', store.getState());
+  return result;
 };
 
 export default [
   thunkMiddleware,
-  myMiddleware,
+  logger,
   loadingBarMiddleware({ promiseTypeSuffixes }),
 ];
 
